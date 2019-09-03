24a730740d4e764e73a85dc34a0280145b781985
markwartman1 <mwartman@d2l.lonestar.edu>
2019-09-02 23:39:34 -0500

    retry of added my first full-stack apps

---
+++ src/js/animate-scroll.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+
+    var target = this.hash; // Stores hash as target
+    var $target = $(target); // Turns target dest. into a jQuery object
+
+    $('html, body').animate({
+      'scrollTop': $target.offset().top
+    }, 1200, 'swing', function() {
+      window.location.hash = target;
+    });
+
+
+  });
+});
